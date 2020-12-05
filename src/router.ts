041126e62ffccb2cc22197238e5be508b875ca31
loganyu <logan0926@163.com>
2020-12-05 14:50:02 +0800

    优化路由及隐藏dist目录

--- src/router.ts
+++ src/router.ts
@@ -5,19 +5,23 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
-import Intro from './views/Intro.vue'
-import Install from './views/Install.vue'
-import GetStarted from './views/GetStarted.vue'
+import Markdown from  './components/Markdown.vue'
+import { h } from 'vue';
+
+import intro from './markdown/intro.md'
+import getStarted from './markdown/get-started.md'
+import install from './markdown/install.md'
 const history =createWebHashHistory()
+const md = string => h(Markdown, { content: string, key: string })
  export  const router=createRouter({
   history:history,
   routes:[
 
     {path:'/', component:Home},
     {path:'/Doc', component:Doc,children:[
-        {path:'intro',component:Intro},
-        {path:'Install',component:Install},
-        {path:'GetStarted',component:GetStarted},
+        { path: "Intro", component: md(intro) },
+        { path: "GetStarted", component: md(getStarted) },
+        { path: "Install", component: md(install) },
         {path:'',redirect:'/doc/Intro'},
         {path:'switch',component:SwitchDemo},
         {path:'Button',component:ButtonDemo},
